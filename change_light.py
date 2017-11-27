0e01954ab40b55343e39dccfeba8926fdb31c5c0
RobJMal <robjmal@gmail.com>
2017-11-26 22:23:48 -0500

    Created HueLightBulb class

--- change_light.py
+++ change_light.py
@@ -1,25 +1,38 @@
 # Functions that will change a single setting on a bulb. Needs URL for it 
-# Might turn this into a class 
+# FILE IS STILL IN ITS PROTOTYPE STAGE. BULB # FOR BRIDGE NEEDS TO BE DYNAMIC  
 
 import requests
 import json 
 
-# Turns on light. 
-def on_light(url):
-	return requests.put(url, data=json.dumps({"on":True}))
+class HueLightBulb:
 
-# Turns off light 
-def off_light(url):
-	return requests.put(url, data=json.dumps({"on":False}))
+	# NEED TO MAKE THE BULB NUMBER DYNAMIC 
+	def __init__(self, url):
+		self.url = url + "/1/state"
 
-# Changes the color of the light by changing the xy value (check Hue Image guide)
-def change_xy(color, url):
-	return requests.put(url, data=json.dumps({"on":True, "xy":color}))
+	# Returns status of bulb 
+	def status(self):
 
-# Changes saturation of bulb 
-def change_sat(sat, url):
-	return requests.put(url, data=json.dumps({"on":True, "sat":sat}))
+		# Removes "/state" at end of url 
+		return request.get(url.replace(" ","")[:-6])
 
-# Changes brightness of bulb
-def change_bri(bri, url):
-	return requests.put(url, data=json.dumps({"on":True, "bri":bri}))
+	# Turns on light. 
+	def on(self):
+		return requests.put(url, data=json.dumps({"on":True}))
+	
+	# Turns off light 
+	def off(self):
+		return requests.put(url, data=json.dumps({"on":False}))
+
+	# Changes the color of the light by changing the xy value (check Hue Image guide)
+	def change_xy(self, color):
+		return requests.put(url, data=json.dumps({"on":True, "xy":color}))
+
+	# Changes saturation of bulb 
+	def change_sat(self, sat):
+		return requests.put(url, data=json.dumps({"on":True, "sat":sat}))
+
+	# Changes brightness of bulb
+	def change_bri(self, bri):
+		return requests.put(url, data=json.dumps({"on":True, "bri":bri}))
+
