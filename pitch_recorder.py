2812211e59bf7c9347cb9b7d7d208109fd0e9c37
RobJMal <robjmal@gmail.com>
2017-12-01 04:31:12 -0500

    Modified file to take pitch and glow

--- pitch_recorder.py
+++ pitch_recorder.py
@@ -7,6 +7,8 @@ from matplotlib.mlab import find
 import pyaudio
 import numpy as np
 import math
+import requests
+import json 
 
 
 chunk = 1024
@@ -36,4 +38,13 @@ frames_per_buffer = chunk)
 for i in range(0, int(RATE / chunk * RECORD_SECONDS)):
     data = stream.read(chunk)
     Frequency=Pitch(data)
+    if 435 <= Frequency <= 445:
+  	    requests.put("http://192.168.1.173/api/99MmFck8z1xaA3jvKD1oJD8wVvYyr3iZdOY4vw1U/lights/1/state", data=json.dumps({"on":True, "hue":65500}))
+    else:
+  	    requests.put("http://192.168.1.173/api/99MmFck8z1xaA3jvKD1oJD8wVvYyr3iZdOY4vw1U/lights/1/state", data=json.dumps({"on":False}))
     print(f"{Frequency} Frequency")
+
+
+requests.put("http://192.168.1.173/api/99MmFck8z1xaA3jvKD1oJD8wVvYyr3iZdOY4vw1U/lights/1/state", data=json.dumps({"on":False}))
+
+
